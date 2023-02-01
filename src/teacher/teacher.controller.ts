50a034ca40d985b3ae2033c5d76565d24225f484
SualaDev <81395134+SualaDev@users.noreply.github.com>
2023-02-01 18:46:47 +0100

    controller feature updated

--- src/teacher/teacher.controller.ts
+++ src/teacher/teacher.controller.ts
@@ -1,17 +1,19 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { FindTeacherResponseDto } from '../teacher/dto/teacher.dto';
+import { TeacherService } from '../teacher/teacher.service';
 
 @Controller('teachers')
 export class TeacherContoller {
+  constructor(private readonly teacherService: TeacherService) {}
   @Get()
   getTeachers(): FindTeacherResponseDto[] {
-    return 'Get All Teachers';
+    return this.teacherService.getTeachers();
   }
 
   @Get('/:TeacherId')
   getTeacherById(
     @Param('TeacherId') teacherId: string,
   ): FindTeacherResponseDto {
-    return `Get Teacher By The Id Of ${teacherId}`;
+    return this.teacherService.getTeacherById(teacherId);
   }
 }
